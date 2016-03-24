2d1e4dbcffa394c104a7f559a4e563f2fbe2a442
André Fatia <alfatia10@gmail.com>
2016-03-24 13:52:32 +0000

    Fixed bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/instal
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install stow
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install stow
+    brew install stow;fi
 
 stow linters
 stow vim
